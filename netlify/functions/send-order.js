d0a05221bf7e487fe345ea83226c862c248575b5
namak-cafe <kian22742274@gmail.com>
2026-06-02 20:36:31 +0330

    Update send-order.js

--- netlify/functions/send-order.js
+++ netlify/functions/send-order.js
@@ -1,32 +1,37 @@
 exports.handler = async (event) => {
-  const data = JSON.parse(event.body);
+  try {
+    const data = JSON.parse(event.body);
 
-  const token = process.env.BOT_TOKEN;
-  const chatId = process.env.CHAT_ID;
+    const token = process.env.BOT_TOKEN;
+    const chatId = process.env.CHAT_ID;
 
-  const text = ☕ سفارش جدید
+    const text = `☕ سفارش جدید
 
 👤 نام: ${data.name}
 🥤 نوشیدنی: ${data.drink}
 🔢 تعداد: ${data.count}
-📝 توضیحات: ${data.note || "ندارد"};
+📝 توضیحات: ${data.note || "ندارد"}`;
 
-  const response = await fetch(
-    https://api.telegram.org/bot${token}/sendMessage,
-    {
+    await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
         chat_id: chatId,
-        text
+        text: text
       })
-    }
-  );
+    });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ ok: true })
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ ok: true })
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.message
+    };
+  }
 };
